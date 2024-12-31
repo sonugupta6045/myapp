136313fc7b286d853b7ba40a65584a96f806a197
sonu <sonugupta6045@gmail.com>
2024-12-31 18:57:44 +0530

    all package install for requied login and singup page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,17 @@
 import Navbar from "./components/landingPage/Navbar";
 import Home from "./pages/Home";
-
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-     <Home/>
      
+     
+     <BrowserRouter>
+     <Routes>
+        <Route path='/landing' element={<Home/>}></Route>
+     </Routes>
+     </BrowserRouter>
     </div>
   );
 }
